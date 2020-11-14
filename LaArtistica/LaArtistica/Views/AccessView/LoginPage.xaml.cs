a2f464f22483024ae2b5879f4631dc513189c64b
Valeria Roias <vrojasc739@ulacit.ed.cr>
2020-11-14 17:12:24 -0600

    Login y Registro

---
+++ LaArtistica/LaArtistica/Views/AccessView/LoginPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LaArtistica.Views.AccessView
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
