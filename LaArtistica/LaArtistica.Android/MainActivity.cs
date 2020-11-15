f8de4641e945b163c94041b2da777b46e8c0ef77
dfajardom494 <dfajardom494@ulacit.ed.cr>
2020-11-15 11:28:47 -0600

    Portada y SQLConfig

--- LaArtistica/LaArtistica.Android/MainActivity.cs
+++ LaArtistica/LaArtistica.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace LaArtistica.Droid
 {
-    [Activity(Label = "LaArtistica", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "LaArtistica", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -21,7 +21,8 @@ namespace LaArtistica.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            string dbPath = FileAccess.GetLocalFilePath("users.db3");
+            LoadApplication(new App(dbPath));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ LaArtistica/LaArtistica/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace LaArtistica.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(50), Unique]
+        public String Username { get; set; }
+
+        [MaxLength(50), Unique]
+        public String Email { get; set; }
+
+        [MaxLength(50)]
+        public String Password { get; set; }
+
+
+
+
+    }
+}
